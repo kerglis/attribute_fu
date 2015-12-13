974792d96d3b10738dc0fec13c8cf78f59a32bd0
Kristaps Erglis <kristaps.erglis@gmail.com>
2015-12-13 12:15:27 +0200

    Make as gem

--- lib/attribute_fu.rb
+++ lib/attribute_fu.rb
@@ -1,2 +1,8 @@
-module AttributeFu #:nodoc:  
+require 'attribute_fu/associations'
+require 'attribute_fu/associated_form_helper'
+
+module AttributeFu #:nodoc:
 end
+
+ActiveRecord::Base.class_eval { include AttributeFu::Associations }
+ActionView::Helpers::FormBuilder.class_eval { include AttributeFu::AssociatedFormHelper }
